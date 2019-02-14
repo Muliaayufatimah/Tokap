3dc8acbe9a305f6aa826ff002028325272d35be9
muliaayu <Fatimahmulia10@gmail.com>
2019-02-14 09:46:47 +0700

    eps11

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -34,22 +34,24 @@
 
 <table class="table table-striped mb-3">
 	<tr>
-		<th>Name</th><th>Email</th><th>&nbsp;</th>
+		<th>Name</th><th>Email</th><th>Akses</th><th>&nbsp;</th>
 	</tr>
 	@foreach($data as $dt)
 	<tr>
 		<td>{{ $dt->name }}</td>
 		<td>{{ $dt->email}}</td>
+		<td>{{ $dt->akses}}</td>
 		<td>
-			<a href="#" class="btn btn-succes btn-sm">
+			<a href="#" class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
 
-			@if( $dt->id != Auth::id() )
-			<button class="btn btn-danger btn-sm" type="button">
-				<i class="fa fa-w fa-trash"></i>
-			</button>
-			@endif
+           @if( $dt->id != Auth::id() )
+           <button class="btn btn-danger btn-sm" type="button">
+           	<i class="fa fa-w fa-trash"></i>
+           	
+           </button>
+           @endif
 
 		</td>
 	</tr>
