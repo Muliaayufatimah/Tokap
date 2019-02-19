05e4a2d7f74850c1eeea1980baeb61a8133828ee
muliaayu <Fatimahmulia10@gmail.com>
2019-02-19 09:29:23 +0700

    eps-17&18

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -13,6 +13,16 @@
 	</div>
 	@endif
 
+
+	@if(session('result') == 'update')
+<div class="alert alert-success alert-dismissible fade show">
+	<strong>Update!</strong> Berhasil Diupadate.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+		</button>
+	</div>
+	@endif
+
 <div class="row">
 	<div class="col-md-6 mb-3">
 		<a href="{{ route('admin.user.add') }}" class="btn btn-primary">[+] Tambah</a>
@@ -58,7 +68,9 @@
 			</a>
 
            @if( $dt->id != Auth::id() )
-           <button class="btn btn-danger btn-sm" type="button">
+           <button class="btn btn-danger btn-sm btn-trash" 
+             data-id="{{ $dt->id}}"
+           type="button">
            	<i class="fa fa-w fa-trash"></i>
            	
            </button>
@@ -73,3 +85,60 @@
 	->links('vendor.pagination.bootstrap-4')
 }}
 @endsection
+
+@push('modal')
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+
+			<div class="modal-header">
+				<h5 class="modal-title">Delete</h5>
+				<button class="close" type="button" data-dismiss="modal">
+					<span>x</span>
+					
+				</button>
+				
+			</div><!--End Modal Header-->
+
+			<div class="modal-body">
+				Apakah anda Yakin Ingin Menghapusnya?
+				<form id="form-delete" method="post" action="#">
+					{{ csrf_field() }}
+					{{ method_field('delete') }}
+					<input type="hidden" name="id" id="input-id">
+        
+					
+				</form>
+					
+					
+				
+			</div><!--End Modal Body-->
+
+			<div class="modal-footer">
+
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <button class="btn btn-primary btn-delete" type="button">Delete</button>				
+			</div>
+			
+		</div><!--End Modal Content-->
+		
+	</div><!-- End Modal Dialog-->
+	
+</div>
+@endpush 
+
+@push('js')
+<script type="text/javascript">
+$(function(){
+	$('.btn-trash').click(function(){
+	id =  $(this).attr('data-id');
+	$('#input-id').val(id);
+	$('#deleteModal').modal('show');
+});
+
+$('.btn-delete').click(function(){
+	alert( $('#input-id').val() );
+});
+})
+</script>
+@endpush
