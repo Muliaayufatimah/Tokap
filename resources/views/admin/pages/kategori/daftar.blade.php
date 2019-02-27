cb4d6c97c3b5a33208115fa0afdbd256cacb7c41
muliaayu <Fatimahmulia10@gmail.com>
2019-02-27 11:44:09 +0700

    eps20

---
+++ resources/views/admin/pages/kategori/daftar.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.main')
+@section('title','kategori')
+@section('content')
+<h1>Kategori</h1>
+<hr>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['middleware'=>['auth']], function(){
-Route::prefix('admin')->group(function(){
+Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
+
 	Route::get('/', function(){
 		return view('admin.pages.dashboard');
 	})->name('admin.home');
 
+
+    /* User */
 	Route::prefix('user')->group(function(){
 		
 		Route::get('/','UserController@daftar')->name('admin.user')->middleware('akses.admin');
+		Route::delete('/','UserController@delete')->middleware('akses.admin');
 
 		Route::get('/add','UserController@add')->name('admin.user.add')->middleware('akses.admin');
 		Route::post('/add','UserController@save')->middleware('akses.admin');
@@ -38,7 +41,13 @@ Route::prefix('admin')->group(function(){
 		Route::get('/setting', 'UserSettingController@form')->name('admin.user.setting');
 		Route::post('/setting','UserSettingController@update');
 	});
+
+     /* kategori */
+
+     Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
+     	Route::get('/','kategoriController@daftar')->name('admin.kategori');
 });
+
 
 });
 
